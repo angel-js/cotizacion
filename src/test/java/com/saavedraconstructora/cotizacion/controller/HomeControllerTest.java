3d6be41c31929510cd26cb4989e53dfbf5ac90c8
angel-js <angelserrano143@gmail.com>
2023-01-29 00:56:39 -0300

    feat(): Unit Test are done!

--- src/test/java/com/saavedraconstructora/cotizacion/controller/HomeControllerTest.java
+++ src/test/java/com/saavedraconstructora/cotizacion/controller/HomeControllerTest.java
@@ -1,29 +1,16 @@
 package com.saavedraconstructora.cotizacion.controller;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.junit.jupiter.api.Assertions.*;
 
+class HomeControllerTest {
 
-@RunWith(SpringRunner.class)
-@WebMvcTest
-public class HomeControllerTest {
-    
-    @Autowired
-    private MockMvc mockMvc;
-    
     @Test
-    public void homeListTest() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("home"));
+    void homeList() {
+        HomeController homeController = new HomeController();
+        String result = homeController.homeList();
+        assertEquals("home", result);
     }
-    
 }
--- src/test/java/com/saavedraconstructora/cotizacion/service/HomeServiceTest.java
+++
@@ -1,5 +0,0 @@
-package com.saavedraconstructora.cotizacion.service;
-
-public class HomeServiceTest {
-    
-}
