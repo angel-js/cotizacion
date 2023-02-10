b84ffcd4bd9321fc9e611f9d05accf6afd5ce74a
angel-js <angelserrano143@gmail.com>
2023-02-10 18:16:08 -0300

    feat(): First Functional Spring Security

--- src/main/java/com/saavedraconstructora/cotizacion/controller/HomeController.java
+++ src/main/java/com/saavedraconstructora/cotizacion/controller/HomeController.java
@@ -1,26 +1,11 @@
 package com.saavedraconstructora.cotizacion.controller;
 
-import com.saavedraconstructora.cotizacion.model.Authority;
-import com.saavedraconstructora.cotizacion.model.Usuario;
 import com.saavedraconstructora.cotizacion.service.UsuarioService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Collection;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -28,8 +13,6 @@ public class HomeController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
     private static final Logger log = LoggerFactory.getLogger(HomeController.class);
     @RequestMapping("/home")
     public String homeList() {
