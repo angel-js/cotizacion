9ee905a0907ac390fcae0380f46b311458e66efa
Angel Serrano <angelserrano143@gmail.com>
2023-03-02 12:26:17 -0300

    feat(): Inicio Reporte

--- src/main/java/com/saavedraconstructora/cotizacion/controller/AdminController.java
+++ src/main/java/com/saavedraconstructora/cotizacion/controller/AdminController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/admin/users")
 public class AdminController {
 
